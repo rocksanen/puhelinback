a730785f827190ff6af1c120e7ad59183b3fb20f
rocksanen <rocksanen@gmail.com>
2022-11-30 23:08:36 +0200

    com

--- mongo.js
+++ mongo.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
+/*
 if(process.argv.length < 3) {
 
     console.log('give password as argument');
     process.exit(1)
 }
+*/
 
 //const password = process.argv[2]
 
